88a10433a270be319982e3ff4afe5b6d939fd156
Onyinyechukwu Adesanya <yinadesanya@gmail.com>
2023-07-18 21:52:17 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl bg-purple-600">Getting Started</h1>
+      <button className="btn text-3xl">Button</button>
+    </>
+  );
+}
+
+export default App;
